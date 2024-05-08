0de698ef14c42b131ddfd35bfcad01930d80f7c4
pmallol <paulamallol@gmail.com>
2024-05-08 19:31:55 -0300

    Init Repo

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Sakura Coffee beans',
+      tasteNotes: ['cherry', 'almond', 'chocolate'],
+      price: 19.00,
+    }
+  }
+})
