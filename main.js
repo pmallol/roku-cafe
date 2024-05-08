0a7054fc1bbb36de273b0c677d86efba666f89a5
pmallol <paulamallol@gmail.com>
2024-05-08 19:56:16 -0300

    style: Update navigation and product display in index.html

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: 'Sakura Coffee beans',
+      productName: 'Sakura Coffee beans',
       tasteNotes: ['cherry', 'almond', 'chocolate'],
       price: 19.00,
+      image: './assets/images/De-Mello-coffee-sakura-227g.webp',
     }
   }
 })
