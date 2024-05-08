68d4f7d1533083c29a7cb2d5c768ea6be9ece8c7
pmallol <paulamallol@gmail.com>
2024-05-08 20:06:02 -0300

    refactor: Improve product display and inventory management

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const app = Vue.createApp({
       tasteNotes: ['cherry', 'almond', 'chocolate'],
       price: 19.00,
       image: './assets/images/De-Mello-coffee-sakura-227g.webp',
+      inventory: 58,
+      onSale: true,
+      variants: [
+        { id: 1, size:'500gr'},
+        { id: 2, size:'1kg'},
+      ]
     }
   }
 })
