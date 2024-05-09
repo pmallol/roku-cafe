5c05d94a1efb57015d931cfd704b8069604e4d2c
pmallol <paulamallol@gmail.com>
2024-05-08 23:01:53 -0300

    chore: Update product price display and add final price calculation

--- main.js
+++ main.js
@@ -6,12 +6,20 @@ const app = Vue.createApp({
       tasteNotes: ['cherry', 'almond', 'chocolate'],
       price: 19.00,
       image: './assets/images/De-Mello-coffee-sakura-227g.webp',
-      inventory: 58,
       onSale: true,
       variants: [
-        { id: 1, size:'226gr'},
-        { id: 2, size:'500gr'},
+        { id: 1, size:'226gr', quantity: 18},
+        { id: 2, size:'500gr', quantity: 9},
       ]
+    }
+  },
+  computed: {
+    finalPrice() {
+      if (this.onSale) {
+        const discount = this.price * 0.2
+        return this.price - discount
+      }
+      return this.price
     }
   },
   methods: {
