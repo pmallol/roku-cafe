33e39897df1ce7d0af2389f90b5b59db5025fd64
pmallol <paulamallol@gmail.com>
2024-05-08 22:57:24 -0300

    feature: Add an add to cart button

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart:0,
       productName: 'Sakura Coffee beans',
       tasteNotes: ['cherry', 'almond', 'chocolate'],
       price: 19.00,
@@ -8,9 +9,14 @@ const app = Vue.createApp({
       inventory: 58,
       onSale: true,
       variants: [
-        { id: 1, size:'500gr'},
-        { id: 2, size:'1kg'},
+        { id: 1, size:'226gr'},
+        { id: 2, size:'500gr'},
       ]
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
     }
   }
 })
